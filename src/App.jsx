f38473900ebd0161ec6c43675a7dddd2abf4f4e8
aretha <myaretha2620@gmail.com>
2023-07-06 12:14:37 +0300

    added redux store

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header/Header.jsx"
 import Footer from "./components/Footer/Footer.jsx";
 
-
-
 const App = () => {
+
     return (
         <>
             <Header />
